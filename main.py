399ceabbc17ca90546bb0a21b902e5f6ee0076d8
Bil Bas <bil.bagpuss@gmail.com>
2025-07-27 18:09:32 +0100

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts, main.py

--- main.py
+++ main.py
@@ -2,26 +2,36 @@ def render_walls():
     for column in range(18):
         for row in range(18):
             if tiles.tile_at_location_is_wall(tiles.get_tile_location(column, row)):
-                tiles.set_tile_at(tiles.get_tile_location(column, row),
-                    sprites.dungeon.chest_closed)
+                tiles.set_tile_at(tiles.get_tile_location(column, row), sprites.builtin.brick)
             elif tiles.tile_at_location_equals(tiles.get_tile_location(column, row),
-                assets.tile("""
-                    transparency16
-                    """)):
-                pass
+                sprites.dungeon.stair_large):
+                tiles.place_on_tile(Wizard, tiles.get_tile_location(column, row))
             else:
                 pass
+def create_wizard():
+    global wiz
+    wiz = sprites.create(assets.image("""
+        Wiz
+        """), SpriteKind.player)
+    controller.move_sprite(wiz, 60, 60)
+    info.set_life(3)
+    info.set_score(0)
+    scene.camera_follow_sprite(wiz)
+    return wiz
 
 def on_overlap_tile(sprite, location):
-    tiles.set_tile_at(location, assets.tile("""
-        transparency16
-        """))
+    tiles.set_tile_at(location, sprites.dungeon.chest_open)
+    info.change_score_by(10)
+    music.play(music.melody_playable(music.ba_ding),
+        music.PlaybackMode.IN_BACKGROUND)
 scene.on_overlap_tile(SpriteKind.player,
     sprites.dungeon.chest_closed,
     on_overlap_tile)
 
-Wizard = sprites.create(assets.image("""Wiz"""), SpriteKind.player)
-controller.move_sprite(Wizard, 60, 60)
-scene.camera_follow_sprite(Wizard)
-tiles.set_current_tilemap(tilemap("""level1"""))
+wiz: Sprite = None
+Wizard: Sprite = None
+tiles.set_current_tilemap(tilemap("""
+    level1
+    """))
+Wizard = create_wizard()
 render_walls()
--- main.ts
+++ main.ts
@@ -1,29 +1,58 @@
+scene.onHitWall(SpriteKind.Player, function (sprite, location) {
+    if (tiles.tileAtLocationEquals(location, sprites.dungeon.doorClosedNorth) && Keys >= 1) {
+        tiles.setTileAt(location, sprites.dungeon.doorOpenNorth)
+        music.play(music.melodyPlayable(music.knock), music.PlaybackMode.InBackground)
+        tiles.setWallAt(location, false)
+        Keys += -1
+    }
+})
 function render_walls () {
     for (let column = 0; column <= 17; column++) {
         for (let row = 0; row <= 17; row++) {
             if (tiles.tileAtLocationIsWall(tiles.getTileLocation(column, row))) {
                 tiles.setTileAt(tiles.getTileLocation(column, row), sprites.builtin.brick)
-            } else if (tiles.tileAtLocationEquals(tiles.getTileLocation(column, row), assets.tile`transparency16`)) {
-                tiles.setTileAt(tiles.getTileLocation(0, 0), sprites.builtin.brick)
+            } else if (tiles.tileAtLocationEquals(tiles.getTileLocation(column, row), assets.tile`Stairs down`)) {
+                tiles.placeOnTile(Wizard, tiles.getTileLocation(column, row))
+            } else if (tiles.tileAtLocationEquals(tiles.getTileLocation(column, row), sprites.dungeon.doorClosedNorth)) {
+                tiles.setWallAt(tiles.getTileLocation(column, row), true)
             } else {
-            	
+                tiles.setTileAt(tiles.getTileLocation(column, row), assets.tile`transparency16`)
             }
         }
     }
 }
 function create_wizard () {
-    Wizard = sprites.create(assets.image`Wiz`, SpriteKind.Player)
-    controller.moveSprite(Wizard, 60, 60)
+    wiz = sprites.create(assets.image`Wiz`, SpriteKind.Player)
+    controller.moveSprite(wiz, 60, 60)
     info.setLife(3)
     info.setScore(0)
+    Keys = 0
+    Magic = 0
+    scene.cameraFollowSprite(wiz)
+    return wiz
 }
-scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.chestClosed, function (sprite, location) {
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleBlueCrystal, function (sprite, location) {
     tiles.setTileAt(location, assets.tile`transparency16`)
+    music.play(music.melodyPlayable(music.powerUp), music.PlaybackMode.InBackground)
+    info.changeLifeBy(1)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.chestClosed, function (sprite, location) {
+    tiles.setTileAt(location, sprites.dungeon.chestOpen)
     info.changeScoreBy(10)
     music.play(music.melodyPlayable(music.baDing), music.PlaybackMode.InBackground)
 })
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleRedCrystal, function (sprite, location) {
+    tiles.setTileAt(location, assets.tile`transparency16`)
+    music.play(music.melodyPlayable(music.powerUp), music.PlaybackMode.InBackground)
+    Magic += 1
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.stairLarge, function (sprite, location) {
+    game.gameOver(true)
+})
+let Magic = 0
+let wiz: Sprite = null
+let Keys = 0
 let Wizard: Sprite = null
-scene.cameraFollowSprite(Wizard)
 tiles.setCurrentTilemap(tilemap`level1`)
+Wizard = create_wizard()
 render_walls()
-create_wizard()
